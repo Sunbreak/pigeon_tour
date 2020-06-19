ff5e83c576d56726466d88f90c29f1170e409da2
Sunbreak <sunbreak.wang@gmail.com>
2020-06-20 06:22:08 +0800

    Initial commit
    Signed-off-by: Sunbreak <sunbreak.wang@gmail.com>

---
+++ ios/Classes/PigeonTourPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface PigeonTourPlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/PigeonTourPlugin.m
@@ -0,0 +1,20 @@
+#import "PigeonTourPlugin.h"
+
+@implementation PigeonTourPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"pigeon_tour"
+            binaryMessenger:[registrar messenger]];
+  PigeonTourPlugin* instance = [[PigeonTourPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
